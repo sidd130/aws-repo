e34a7c6714205a5d4e49d819eb02ac060bcbed03
Siddharth Das <siddharth2212@gmail.com>
2024-04-13 14:00:29 +0530

    Updated handler to send back message in the response

--- terraform/Lambda/handler.py
+++ terraform/Lambda/handler.py
@@ -4,7 +4,8 @@ def lambda_handler(event, context):
     print(str(event))
     print(str(context))
     resp = {
-        "status": 200
+        "status": 200,
+        "msg": "Info message from Lambda"
     }
 
     return json.dumps(resp)
