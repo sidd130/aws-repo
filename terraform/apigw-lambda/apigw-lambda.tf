13bf4b81b3fb07edd5b454e27d70c784f64e3ca6
Siddharth Das <siddharth2212@gmail.com>
2024-04-13 21:53:49 +0530

    Added tf config for apigw

---
+++ terraform/apigw-lambda/apigw-lambda.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.16"
+    }
+  }
+
+  required_version = ">=1.2.0"
+}
+
+provider "aws" {
+    region = var.aws_region  
+}
+
+resource "aws_apigatewayv2_api" "apigw-http-api" {
+  name = "apigw-http-api"
+  protocol_type = "HTTP"
+  body = jsonencode(
+    {
+        "openapi": "3.0.1",
+        "info": {
+            "title": "apigw-http-api-openapi-spec",
+            "version": "1.0"
+        },
+        "path": {
+            "/time": {
+                "get": {
+                    "description": "Returns time in format dd-mon-yyyy hh24:mi:ss"
+                }
+            }
+        }
+    }
+  )
+}
