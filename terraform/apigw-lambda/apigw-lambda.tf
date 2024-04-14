16242b1a11b1ea8b0eb26d2bd2feb30ae7a62526
Siddharth Das <siddharth2212@gmail.com>
2024-04-14 13:20:43 +0530

    Updated request parameters in apigw integration

--- terraform/apigw-lambda/apigw-lambda.tf
+++ terraform/apigw-lambda/apigw-lambda.tf
@@ -87,12 +87,14 @@ resource "aws_apigatewayv2_api" "apigw-http-api" {
 resource "aws_apigatewayv2_integration" "apigw-integration" {
   api_id = aws_apigatewayv2_api.apigw-http-api.id
   integration_type = "AWS_PROXY"
-  integration_method = "ANY"
+  integration_method = "POST"
   connection_type = "INTERNET"
   integration_uri = aws_lambda_function.lambda-time.invoke_arn
   payload_format_version = "2.0"
   request_parameters = {
-      "append:header.Content-Type": "application/json"
+      "200" : {
+        "append:header.Content-Type": "application/json"
+      }
   }
   response_parameters {
     status_code = 200
