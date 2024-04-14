0fee76eeb2afe5d67d8ea07e76a738d9644e0ae8
Siddharth Das <siddharth2212@gmail.com>
2024-04-14 16:02:53 +0530

    Updated lambda handler to print event and context

--- terraform/apigw-lambda/handler.py
+++ terraform/apigw-lambda/handler.py
@@ -4,5 +4,7 @@ import datetime
 def lambda_handler(event, context):
     resp = {}
     resp["time"] = str(datetime.datetime.now())
+    print(event)
+    print(context)
     print(resp)
     return json.dumps(resp)
