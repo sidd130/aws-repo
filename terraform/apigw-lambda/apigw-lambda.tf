66556dfb2c114a4cf8100d2d509413750d650023
Siddharth Das <siddharth2212@gmail.com>
2024-04-14 13:18:22 +0530

    Updated integration method to ANY

--- terraform/apigw-lambda/apigw-lambda.tf
+++ terraform/apigw-lambda/apigw-lambda.tf
@@ -87,7 +87,7 @@ resource "aws_apigatewayv2_api" "apigw-http-api" {
 resource "aws_apigatewayv2_integration" "apigw-integration" {
   api_id = aws_apigatewayv2_api.apigw-http-api.id
   integration_type = "AWS_PROXY"
-  integration_method = "POST"
+  integration_method = "ANY"
   connection_type = "INTERNET"
   integration_uri = aws_lambda_function.lambda-time.invoke_arn
   payload_format_version = "2.0"
