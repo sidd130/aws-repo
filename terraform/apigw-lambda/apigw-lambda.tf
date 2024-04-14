f8e72b5cc8e567ea63891939282ed3e18f79b240
Siddharth Das <siddharth2212@gmail.com>
2024-04-14 09:59:50 +0530

    Added response parameter mapping to integration

--- terraform/apigw-lambda/apigw-lambda.tf
+++ terraform/apigw-lambda/apigw-lambda.tf
@@ -91,6 +91,12 @@ resource "aws_apigatewayv2_integration" "apigw-integration" {
   connection_type = "INTERNET"
   integration_uri = aws_lambda_function.lambda-time.invoke_arn
   payload_format_version = "2.0"
+  response_parameters {
+    status_code = 200
+    mappings = {
+      "append:header.Content-Type" = "application/json"
+    }
+  }
 }
 
 # Resource definition for API Gateway Route
