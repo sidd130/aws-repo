ba3de1f647a775fd427180cd97c532df4544abb9
Siddharth Das <siddharth2212@gmail.com>
2025-03-12 00:21:50 +0530

    Added SQS queue definition and lambda handler

---
+++ terraform/sqs-lambda-s3/sqs-lambda-s3.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.41"
+    }
+  }
+
+  required_version = ">=1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# data "aws_iam_policy_document" "lambda-policy" {
+#     statement {
+#       sid = "lambda-policy"
+#       principals {
+#         type = ""
+#       }
+#     }
+# }
+
+# resource "aws_lambda_function" "event-processor" {
+#     function_name = "event-processer"
+#     role = ""
+# }
+
+resource "aws_sqs_queue" "event-collector" {
+  name             = "event-collector-queue"
+  max_message_size = 2048
+}
+
+# resource "aws_sqs_queue_policy" "event-collector-policy" {
+#   queue_url = aws_sqs_queue.event-collector.id
+# }
