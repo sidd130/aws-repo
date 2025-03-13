313dcf6b6e82dc6f49511bdbdc58b82976178197
Siddharth Das <siddharth2212@gmail.com>
2025-03-13 23:43:10 +0530

    Added SQS queue policy

--- terraform/sqs-lambda-s3/sqs-lambda-s3.tf
+++ terraform/sqs-lambda-s3/sqs-lambda-s3.tf
@@ -13,14 +13,19 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# data "aws_iam_policy_document" "lambda-policy" {
-#     statement {
-#       sid = "lambda-policy"
-#       principals {
-#         type = ""
-#       }
-#     }
-# }
+data "aws_iam_policy_document" "sqs-policy-doc" {
+    statement {
+      sid = "sqs-policy-doc"
+      principals {
+        type = "User"
+        identifiers = [ "arn:aws:iam::438801865484:user/sqs_rw_user" ]
+      }
+      actions = ["sqs:SendMessage"]
+      resources = [aws_sqs_queue.event-collector.arn]
+    }
+
+    depends_on = [aws_sqs_queue.event-collector]
+}
 
 # resource "aws_lambda_function" "event-processor" {
 #     function_name = "event-processer"
@@ -32,6 +37,7 @@ resource "aws_sqs_queue" "event-collector" {
   max_message_size = 2048
 }
 
-# resource "aws_sqs_queue_policy" "event-collector-policy" {
-#   queue_url = aws_sqs_queue.event-collector.id
-# }
+resource "aws_sqs_queue_policy" "event-collector-policy" {
+  queue_url = aws_sqs_queue.event-collector.id
+  policy = data.aws_iam_policy_document.sqs-policy-doc.json
+}
