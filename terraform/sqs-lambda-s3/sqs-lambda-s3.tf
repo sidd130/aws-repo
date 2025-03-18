a081a0657adecb5754cf7539f80eb9818cb432aa
Siddharth Das <siddharth2212@gmail.com>
2025-03-19 00:39:50 +0530

    Switched to iam policy document for s3 bucket policy

--- terraform/sqs-lambda-s3/sqs-lambda-s3.tf
+++ terraform/sqs-lambda-s3/sqs-lambda-s3.tf
@@ -145,31 +145,20 @@ resource "aws_s3_bucket" "event-storage" {
   }
 }
 
-# data "aws_iam_policy_document" "bucket-policy" {
-#   statement {
-#     effect = "Allow"
-#     actions = ["s3:PutObject"]
-#     principals {
-#       type = "AWS"
-#       identifiers = [
-#         aws_lambda_function.event-processor.arn
-#       ]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "bucket-policy" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
 
 resource "aws_s3_bucket_policy" "event-storage-bucket-policy" {
   bucket = aws_s3_bucket.event-storage.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "s3:PutObject"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.bucket-policy.json
 }
